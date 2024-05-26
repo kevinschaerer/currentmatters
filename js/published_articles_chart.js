1804076ab4a0538658059fe050dca532be9a9ce6
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-05-26 09:05:07 +0200

    Remove legend of chart

--- js/published_articles_chart.js
+++ js/published_articles_chart.js
@@ -33,59 +33,59 @@ function processData(todayData, yesterdayData, twoDaysAgoData, threeDaysAgoData,
 
   // Define the chart
   new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Today', 'Yesterday', 'Wednesday', 'Tuesday', 'Monday', 'Sunday', 'Saturday'],
-          datasets: [
-              {
-                  label: 'Published Articles',
-                  data: [todayCount, yesterdayCount, twoDaysAgoCount, threeDaysAgoCount, fourDaysAgoCount, fiveDaysAgoCount, sixDaysAgoCount],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                      'rgba(54, 162, 235, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)',
-                      'rgba(54, 162, 235, 1)'
-                  ],
-                  borderWidth: 1
-              }
-          ],
-      },
-      options: {
-        responsive: true,
-        animation: {
-          duration: 0
-      },
-          maintainAspectRatio: false,
-          plugins: {
-              legend: {
-                  display: false
-              },
-          },        
-          scales: {
-              y: {
-                  beginAtZero: true,
-                  grid: {
-                      color: '#252525',
-                  }
-              },
-              x: {
-                  grid: {
-                      color: '#252525',
-                  }
-              },
-          }
-      }
-  });
+    type: 'bar',
+    data: {
+        labels: ['Today', 'Yesterday', 'Wednesday', 'Tuesday', 'Monday', 'Sunday', 'Saturday'],
+        datasets: [
+            {
+                label: 'Published Articles',
+                data: [todayCount, yesterdayCount, twoDaysAgoCount, threeDaysAgoCount, fourDaysAgoCount, fiveDaysAgoCount, sixDaysAgoCount],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }
+        ],
+    },
+    options: {
+      responsive: true,
+      animation: {
+        duration: 0
+    },
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
+            },
+        },        
+        scales: {
+            y: {
+                beginAtZero: true,
+                grid: {
+                    color: '#252525',
+                }
+            },
+            x: {
+                grid: {
+                    color: '#252525',
+                }
+            },
+        }
+    }
+});
 }
